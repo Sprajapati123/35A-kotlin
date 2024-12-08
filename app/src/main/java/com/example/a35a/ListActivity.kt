7a72bfba9bc2d35703079c5a3a2cf4258372cb02
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-08 08:44:55 +0545

    listview

---
+++ app/src/main/java/com/example/a35a/ListActivity.kt
@@ -0,0 +1,62 @@
+package com.example.a35a
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ListActivity : AppCompatActivity() {
+    lateinit var listView: ListView
+
+    val contacts = arrayOf(
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+        "Ram", "shyam", "hari", "sita", "gita",
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list)
+        listView = findViewById(R.id.lstView)
+
+        val adapter = ArrayAdapter(
+            this@ListActivity,
+            android.R.layout.simple_list_item_1,
+            contacts
+        )
+
+        listView.adapter = adapter
+        listView.setOnItemClickListener {
+                      adapterView, view, position, l ->
+            Toast.makeText(
+                this@ListActivity,
+                adapterView.getItemAtPosition(position).toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
