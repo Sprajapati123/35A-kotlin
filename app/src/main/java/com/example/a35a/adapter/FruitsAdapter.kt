d95b72351ca4d691a8ee564304753b2945a60cc0
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-11 10:38:15 +0545

    recycler view

--- app/src/main/java/com/example/a35a/adapter/FruitsAdapter.kt
+++ app/src/main/java/com/example/a35a/adapter/FruitsAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.a35a.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +14,24 @@ class FruitsAdapter(
     val imageList : ArrayList<Int>,
     val titleList : ArrayList<String>,
     val descList : ArrayList<String>,
-) {
+) : RecyclerView.Adapter<FruitsAdapter.FruitsViewHolder>() {
     class FruitsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         var image : ImageView = itemView.findViewById(R.id.imageRecycler)
         var title : TextView = itemView.findViewById(R.id.lblTitle)
         var desc : TextView = itemView.findViewById(R.id.lblDescription)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitsViewHolder {
+        val itemView : View = LayoutInflater.from(context).inflate(R.layout.sample_fruits,parent,false)
+        return FruitsViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    override fun onBindViewHolder(holder: FruitsViewHolder, position: Int) {
+
+    }
 }
+
