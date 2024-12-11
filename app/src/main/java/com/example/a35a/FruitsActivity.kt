6b94dcac7b9811a5b04819ba3b17c0cf4babe4d9
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-11 10:55:05 +0545

    adapter

--- app/src/main/java/com/example/a35a/FruitsActivity.kt
+++ app/src/main/java/com/example/a35a/FruitsActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a35a.adapter.FruitsAdapter
 
 class FruitsActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     var imageList = ArrayList<Int>()
     var titleList = ArrayList<String>()
     var descList = ArrayList<String>()
+    lateinit var adapter: FruitsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,7 +35,13 @@ class FruitsActivity : AppCompatActivity() {
         descList.add("This is grapes")
         descList.add("This is mango")
 
+        adapter = FruitsAdapter(
+            this@FruitsActivity,
+            imageList, titleList, descList
+        )
 
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this@FruitsActivity)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
