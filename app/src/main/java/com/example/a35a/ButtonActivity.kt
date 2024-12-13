dac7d541aeddb5868a885946e7ad11dcaab61a45
sprajapati123 <sandisprajapati52@gmail.com>
2024-12-13 08:35:15 +0545

    intent

---
+++ app/src/main/java/com/example/a35a/ButtonActivity.kt
@@ -0,0 +1,47 @@
+package com.example.a35a
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.a35a.databinding.ActivityButtonBinding
+
+class ButtonActivity : AppCompatActivity() {
+    lateinit var binding: ActivityButtonBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityButtonBinding.inflate(layoutInflater)
+       setContentView(binding.root)
+
+
+        binding.btnNavigate.setOnClickListener {
+
+            val username : String = binding.editUsername.text.toString()
+            val password: String = binding.editPassword.text.toString()
+
+            if(username.isEmpty()){
+                binding.editUsername.error = "username can't be empty"
+            }else if(password.isEmpty()){
+                binding.editPassword.error = "password can't be empty"
+            }else{
+                val intent = Intent(this@ButtonActivity,DestinationActivity::class.java)
+                intent.putExtra("username",username)
+                intent.putExtra("password",password)
+                startActivity(intent)
+            }
+
+
+
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/a35a/FruitsActivity.kt
+++ app/src/main/java/com/example/a35a/FruitsActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.a35a.adapter.FruitsAdapter
@@ -41,8 +42,15 @@ class FruitsActivity : AppCompatActivity() {
         )
 
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this@FruitsActivity)
 
+        //vertical scroll
+//        recyclerView.layoutManager = LinearLayoutManager(this@FruitsActivity)
+
+//Horizontal scroll
+//        recyclerView.layoutManager = LinearLayoutManager(this@FruitsActivity,LinearLayoutManager.HORIZONTAL,false)
+
+//Grid
+        recyclerView.layoutManager = GridLayoutManager(this@FruitsActivity, 2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
